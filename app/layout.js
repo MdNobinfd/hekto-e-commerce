f4097c09cdabfca409a188b72a0563e8127c1e3c
MdNobinfd <mdnobin.fdr@gmail.com>
2024-10-02 13:59:21 +0600

    skdjfl

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
-import localFont from "next/font/local";
 import "./globals.css";
+import { Josefin_Sans } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400"],
 });
 
 export const metadata = {
@@ -20,11 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${josefin.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import { Lato } from "next/font/google";
+
+const lato = Lato({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function Home() {
   return (
-    <h1>hello world</h1>
+    <>
+      <h1 className={`font-bold`}>hello world</h1>
+      <h3 className={`${lato.className} font-semibold`}>This is Our Contury</h3>
+    </>
   );
 }
