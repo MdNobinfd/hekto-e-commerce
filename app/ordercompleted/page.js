7e53fe1aa1e5969d1b741ae454856d494b0f15d2
MdNobinfd <mdnobin.fdr@gmail.com>
2025-08-15 16:09:42 +0600

    order complated  page updated some feature

--- app/ordercompleted/page.js
+++ app/ordercompleted/page.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import OrderComplete from '../components/OrderComplete'
+// this is ordercomplated page
+import React from "react";
+import OrderComplete from "../components/OrderComplete";
 
 const page = () => {
   return (
     <>
-    <OrderComplete />
+      <OrderComplete />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
